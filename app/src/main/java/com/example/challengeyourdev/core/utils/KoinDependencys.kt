8618a63dbc49ea0ccd6f69ad35004f4a3505a085
art2202 <arthurrodrigues29@gmail.com>
2020-09-10 19:27:49 -0300

    Adicionando dependencias pra fazer requisição

--- app/src/main/java/com/example/challengeyourdev/core/utils/KoinDependencys.kt
+++ app/src/main/java/com/example/challengeyourdev/core/utils/KoinDependencys.kt
@@ -1,22 +1,31 @@
 package com.example.challengeyourdev.core.utils
 
+import com.example.challengeyourdev.data.api.ApiService
 import com.example.challengeyourdev.data.api.RestApi
+import com.example.challengeyourdev.data.data_sources.MovieRemoteDataSourceImpl
+import com.example.challengeyourdev.data.data_sources.MovieRemoteDataSource
 import com.example.challengeyourdev.data.repositories.MovieRepositoryImpl
 import com.example.challengeyourdev.domain.repositories.MovieRepository
 import com.example.challengeyourdev.domain.usecases.GetAllMovies
+import com.example.challengeyourdev.presentation.pages.movies.MoviesViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 //criado por arthur rodrigues
 
 val modules = module {
-    single { RestApi() }
-    single { MovieRepositoryImpl() }
-    single { GetAllMovies(get()) }
 
-    single<MovieRepository> { MovieRepositoryImpl() }
+
+    single<ApiService> {  RestApi.getRetrofit().create(ApiService::class.java) }
+
+    single { MovieRepositoryImpl(get()) }
+    single { GetAllMovies(get()) }
+    single<MovieRemoteDataSource> { MovieRemoteDataSourceImpl(get()) }
+    single<MovieRepository> { MovieRepositoryImpl(get()) }
 }
 
 val viewModelModule = module {
 
+    viewModel { MoviesViewModel(get()) }
 
 }
