1d56fa73631514b8f283a5cace388a5b47652b83
art2202 <arthurrodrigues29@gmail.com>
2020-09-10 16:58:35 -0300

    Adicionando UseCase para pegar todos os filmes

---
+++ app/src/main/java/com/example/challengeyourdev/domain/usecases/GetAllMovies.kt
@@ -0,0 +1,12 @@
+package com.example.challengeyourdev.domain.usecases
+
+import com.example.challengeyourdev.domain.entities.Movie
+import com.example.challengeyourdev.domain.repositories.MovieRepository
+
+
+//criado por arthur rodrigues
+
+class GetAllMovies(private val movieRepository: MovieRepository) {
+    
+    suspend operator fun invoke() : List<Movie> = movieRepository.getAllMovies()
+}
