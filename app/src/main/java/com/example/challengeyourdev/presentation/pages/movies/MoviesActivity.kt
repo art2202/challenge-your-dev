034b601e54a3f3a2de150de3667004d928fa93c5
art2202 <arthurrodrigues29@gmail.com>
2020-09-10 14:12:38 -0300

    Criando view model do movies

--- app/src/main/java/com/example/challengeyourdev/presentation/pages/movies/MoviesActivity.kt
+++ app/src/main/java/com/example/challengeyourdev/presentation/pages/movies/MoviesActivity.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.challengeyourdev.R
 
-class MainActivity : AppCompatActivity() {
+//criado por arthur rodrigues
+
+class MoviesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movies)
     }
 }
--- app/src/main/java/com/example/challengeyourdev/presentation/pages/movies/MoviesViewModel.kt
+++ app/src/main/java/com/example/challengeyourdev/presentation/pages/movies/MoviesViewModel.kt
@@ -1,7 +1,27 @@
 package com.example.challengeyourdev.presentation.pages.movies
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.challengeyourdev.core.utils.Response
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 
 //criado por balanike
 
-class MoviesViewModel {
+class MoviesViewModel : ViewModel() {
+
+    private val response = MutableLiveData<Response>()
+
+
+    fun getMovies(){
+        CoroutineScope(Dispatchers.IO).launch {
+
+        }
+    }
+
+    fun response(): MutableLiveData<Response> {
+        return response
+    }
 }
