0fb0a1a3d2da1cf3c69c1b08cc7e849e09b49ef9
art2202 <arthurrodrigues29@gmail.com>
2020-09-11 18:25:59 -0300

    Mapper movie data response to movie (domain)

---
+++ app/src/main/java/com/example/challengeyourdev/data/mappers/MovieDataResponseToMovieMapper.kt
@@ -0,0 +1,21 @@
+package com.example.challengeyourdev.data.mappers
+
+import com.example.challengeyourdev.data.models.MovieDataResponse
+import com.example.challengeyourdev.domain.entities.Movie
+
+
+//criado por arthur rodrigues
+
+class MovieDataResponseToMovieMapper : Mapper<MovieDataResponse, Movie>{
+    override fun map(input: MovieDataResponse): Movie = with(input) {
+        Movie(
+            display_title ?: "",
+            byline ?: "",
+            headline ?: "",
+            summary_short ?: "",
+            publication_date ?: "",
+            link?.toEntity()!!,
+            multimedia?.toEntity()!!
+        )
+    }
+}
