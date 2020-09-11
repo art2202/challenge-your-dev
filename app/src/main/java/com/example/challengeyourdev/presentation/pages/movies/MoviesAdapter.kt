2d8685de506e22e794def9fcb32c2a060a724747
art2202 <arthurrodrigues29@gmail.com>
2020-09-10 22:38:48 -0300

    Adicionando Picasso e configurando o movie adapter

--- app/src/main/java/com/example/challengeyourdev/presentation/pages/movies/MoviesAdapter.kt
+++ app/src/main/java/com/example/challengeyourdev/presentation/pages/movies/MoviesAdapter.kt
@@ -1,25 +1,34 @@
 package com.example.challengeyourdev.presentation.pages.movies
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.challengeyourdev.R
 import com.example.challengeyourdev.domain.entities.Movie
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_movie.view.*
 
 
-//criado por balanike
+//criado por arthur rodrigues
 
 class MoviesAdapter(private var listMovies : ArrayList<Movie>) : RecyclerView.Adapter<MoviesAdapter.MyViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false))
     }
 
     override fun getItemCount() = listMovies.size
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val item = listMovies[position]
+
+        holder.itemView.tv_title.text = item.movieTitle
+        Picasso.get().load(item.photoUrl).into(holder.itemView.iv_picture)
+
+    }
 
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
